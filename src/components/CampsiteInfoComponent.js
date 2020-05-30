7ea68253b87148518ada8e970f023d68acf184ed
Hung Le <hple2002@gmail.com>
2020-05-30 12:31:02 -0400

    end of week 2 React

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
 
 
 
@@ -11,11 +11,44 @@ class CampsiteInfo extends Component {
         };
     }
 
+
+    renderComments(comments) {
+        if(comments) {
+            return(
+                <div className="col-md-5 m-1">
+                    <h4>Comments</h4>
+                    {comments.map(comment => (<p key={comment.id}>{comment.text} <br /> -- {comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>))}
+                </div>
+            );
+        }
+        return <div/>;
+    }
+
+    renderCampsite(campsite) {
+
+        return (
+            <div className="col-md-5 m-1">          
+                <Card>
+                    <CardImg top src={campsite.image} alt={campsite.name} />
+                    <CardBody>
+                        <CardTitle>{campsite.name}</CardTitle>
+                        <CardText>{campsite.description}</CardText>
+                    </CardBody>
+                </Card>
+            </div>
+        );
+        
+    }
+
+
     render() {
 
         if (this.props.campsite !== null) {
             return (
-                <div className="row" />
+                <div className="row">
+                    {this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
+                </div>
             );
         }
         return <div />;
