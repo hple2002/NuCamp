bc779b66bdc6ab2b354107d1fcba9c107a99e948
Hung Le <hple2002@gmail.com>
2020-06-03 23:45:37 -0400

    Week 3 after reorganize into MainComponent

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -21,7 +21,7 @@ class CampsiteInfo extends Component {
                 </div>
             );
         }
-        return <div/>;
+        return <div />;
     }
 
     renderCampsite(campsite) {
@@ -42,17 +42,17 @@ class CampsiteInfo extends Component {
 
 
     render() {
-
-        if (this.props.campsite !== null) {
+        if (this.props.campsite) {
             return (
-                <div className="row">
-                    {this.renderCampsite(this.props.campsite)}
-                    {this.renderComments(this.props.campsite.comments)}
+                <div className="container">
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
             );
         }
         return <div />;
-
     }
 
 } 
