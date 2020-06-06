03b7616d8eac9a6485e8733cd614ee26b34a545e
Hung Le <hple2002@gmail.com>
2020-06-06 10:23:13 -0400

    week 3 end of task 1

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-
 function About(props) {
 
     const partners = props.partners.map(partner => {
