adaf64d2c656fcdc7fd0ea08bf024e5448ce1102
Hung Le <hple2002@gmail.com>
2020-06-20 11:44:29 -0400

    week 5 end of task 1

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { baseUrl } from '../shared/baseUrl';
+import { Loading } from './LoadingComponent';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
+
+function PartnerList (props) {
+    const partners = props.partners.map(partner => {
+        return (
+            <Media tag="li" key="{partner.id}">
+                <RenderPartner partner={partner} />
+            </Media>
+        );
+    });
+
+    if (props.isLoading) {
+        return (
+                <Loading />
+        );
+    }
+    if (props.errMess) {
+        return (
+            <div className="col">
+
+                    <h4>{props.errMess}</h4>
+
+            </div>
+        );
+    }
+    return (
+        <div className="col mt-4">
+        <Media list>
+            {partners}
+        </Media>
+    </div>
+    )
+}
 
 function RenderPartner({partner}) {
     if(partner) {
         return(
             <React.Fragment>
-                <Media object src={partner.image} width="150" alt={partner.name} />
+                <Media object src={baseUrl + partner.image} width="150" alt={partner.name} />
                 <Media body className="ml-5 mb-4"> 
                     <Media heading>
                         {partner.name}
@@ -21,13 +56,7 @@ function RenderPartner({partner}) {
 
 function About(props) {
 
-    const partners = props.partners.map(partner => {
-        return (
-            <Media tag="li" key="{partner.id}">
-                <RenderPartner partner={partner} />
-            </Media>
-        );
-    });
+
 
     return (
         <div className="container">
@@ -82,9 +111,7 @@ function About(props) {
                     <h3>Community Partners</h3>
                 </div>
                 <div className="col mt-4">
-                    <Media list>
-                        {partners}
-                    </Media>
+                    <PartnerList partners={props.partners} />
                 </div>
             </div>
         </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -6,7 +6,7 @@ import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
 import About from './AboutComponent';
-import { postComment, fetchCampsites, fetchComments, fetchPromotions } from '../redux/ActionCreators';
+import { postComment, fetchCampsites, fetchComments, fetchPromotions, fetchPartners  } from '../redux/ActionCreators';
 import { Switch, Route, Redirect, withRouter } from 'react-router-dom'
 import { connect } from 'react-redux';
 import { actions } from 'react-redux-form';
@@ -27,7 +27,8 @@ const mapDispatchToProps = {
     fetchCampsites: () => (fetchCampsites()),
     resetFeedbackForm: () => (actions.reset('feedbackForm')),
     fetchComments: () => (fetchComments()),
-    fetchPromotions: () => (fetchPromotions())
+    fetchPromotions: () => (fetchPromotions()),
+    fetchPartners: () => (fetchPartners())
 };
 
 
@@ -37,6 +38,7 @@ class Main extends Component {
         this.props.fetchCampsites();
         this.props.fetchComments();
         this.props.fetchPromotions();
+        this.props.fetchPartners();
     }
 
     render() {
@@ -47,10 +49,15 @@ class Main extends Component {
                     campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
                     campsitesLoading={this.props.campsites.isLoading}
                     campsitesErrMess={this.props.campsites.errMess}
-                    partner={this.props.partners.filter(partner => partner.featured)[0]}
+                    
                     promotion={this.props.promotions.promotions.filter(promotion => promotion.featured)[0]}
                     promotionLoading={this.props.promotions.isLoading}
                     promotionErrMess={this.props.promotions.errMess}
+
+                    partner={this.props.partners.partners.filter(partner => partner.featured)[0]}
+                    partnerLoading={this.props.partners.isLoading}
+                    partnerErrMess={this.props.partners.errMess}
+
                 />
             );
         }
@@ -76,7 +83,7 @@ class Main extends Component {
                             <Route path='/home' component={HomePage} />
                             <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
                             <Route path='/directory/:campsiteId' component={CampsiteWithId} />
-                           <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} /> } />
+                            <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} /> } />
                             <Route exact path='/aboutus' render={() => <About partners={this.props.partners} /> } />
                             <Redirect to='/home' />
                         </Switch>
